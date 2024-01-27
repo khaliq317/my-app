1fc37766fb76d2abe61567749fef1514f3448d0c
Khaliq Abbas <140581171+khaliq317@users.noreply.github.com>
2024-01-27 11:22:28 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-  <h1>Hello My name is khaliq abbas</h1>
+  <h1>Hello this is my second project that i created with next.js</h1>
     </main>
   );
 }
